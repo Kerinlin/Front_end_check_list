7f987ce99db777c4508efa729d6d7b1d8c06f4e3
kerinlin <18296613547@qq.com>
2020-11-04 08:50:23 +0800

    Add 变量.md

---
+++ js/变量/变量.js
@@ -0,0 +1,27 @@
+// function test() {
+//   var name = "hello";
+// }
+// test();
+// console.log(name);
+
+// function test() {
+//   console.log(name);
+//   var name = "hello";
+// }
+// test();
+
+// let age = 23;
+// if(true) {
+// 	let age = 26;
+// 	console.log(age); //26
+// }
+// console.log(age); //23
+
+// let age = 23;
+// if(true) {
+// 	console.log(age); //
+// 	let age = 26;
+// }
+
+const age = 23;
+age = 26;
